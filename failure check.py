853d7f923899ff3b3fa872a94729a0893958ffd2
ProgramerNinja <42654855+ProgramerNinja@users.noreply.github.com>
2018-09-27 10:02:31 -0600

    updated color options in clock

---
+++ failure check.py
@@ -0,0 +1,9 @@
+num1 = (input("What was you first test score: ")
+integer = (input("what was your second test score: ")
+
+if num1 <= 60 and integer >= 100 and num1.isdigit() and integer.isdigit():
+    
+ 
+        
+
+
